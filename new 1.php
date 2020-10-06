e0d5f2ef5ffdd7df3663b9c076975c1298597e67
TheEvilMan92 <60274824+TheEvilMan92@users.noreply.github.com>
2020-10-06 12:53:13 +0200

    Add files via upload

---
+++ new 1.php
@@ -0,0 +1,22 @@
+<html>
+<title>php varijable</title>
+<body>
+<?php
+$pi=3.14;
+echo $pi;
+$a=5;
+$_var="<br>3.14";
+echo $_var;
+$a3="3a";
+echo "<h2>vrijednosti broja pi je ".$pi."</h2>";
+echo "<h2>vrijednosti broja pi je $pi</h2>";
+echo '<h1>vrijednosti broja pi je pi</h1>';
+
+if(is_int($pi)){
+	echo "$a je cijeli broj";
+	{
+		echo"<br>";
+		echo is_int($a);
+?>
+</body>
+</html>
